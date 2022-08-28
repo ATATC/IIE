11f1c542f68aa3a7b0a02a7b8a4e95b2004bc385
ATATC <futerry@outlook.com>
2022-08-29 04:26:35 +0800

    Index page added.

--- en-src/assets/js/info.js
+++ en-src/assets/js/info.js
@@ -1,3 +1,7 @@
 console.log("Developers: Terry Fu.");
 console.log("Bug report: info@inteledu.ca.");
+$("head").append(
+    $("<link rel='shortcut icon' href='assets/img/logo.png'>"),
+    $("<script src='https://kit.fontawesome.com/7939fa6676.js' crossorigin='anonymous'></script>"),
+);
 initialize();
